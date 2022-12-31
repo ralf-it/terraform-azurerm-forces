5ed0118a4680344de19dbca8b0258d3dd2bd26a1
Ghost / RALF-IT <ghost@ralf-it.com>
2022-12-31 23:59:59 +0100

    Update version to 1.0.30

---
+++ modules/az.context.update/variables.tf
@@ -0,0 +1,28 @@
+variable "configs" {
+  type = any
+}
+
+variable "playbook_version" {
+  type    = string
+  default = null
+}
+variable "playbook_name" {
+  type    = string
+  default = null
+}
+variable "module_name" {
+  type    = string
+  default = null
+}
+variable "module_path" {
+  type    = string
+  default = null
+}
+
+variable "tags" {
+  type    = any
+  default = {}
+}
+variable "location" {
+  default = null
+}
